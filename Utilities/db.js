68bb005849af8dbcb78e2f0eb3c6e4e219ce2e0f
Llano <zache94@hotmail.com>
2017-07-13 14:53:07 +0200

    Added /server url to be able to see a server list now

--- Utilities/db.js
+++ Utilities/db.js
@@ -1,11 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
+var config = require('../config');
 
 var _db;
 
 module.exports = {
 
   connectToServer: function( callback ) {
-    MongoClient.connect( "mongodb://master:zachariaserik@192.168.1.9:27017/monithor?authSource=admin", function( err, db ) {
+    MongoClient.connect( "mongodb://master:zachariaserik@" + config.db_ip + ":27017/monithor?authSource=admin", function( err, db ) {
       _db = db;
       return callback( err );
     } );
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var config = {};
+config.db_ip = "94.255.142.118";
 config.jwt = "qC9DiPSbUZVklJpH0pE8rE3hJgCyfL6b2bb3JYvS==";
 
 module.exports = config;
